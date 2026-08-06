7d73137f5206d20a60a92a0ba1375c3a302a7c9e
andikesawa <andikesawa05@gmail.com>
2026-08-06 23:37:00 +0800

    feature: setup/membuat halaman home dengan metode blade templating engine, dan blade components

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/blog', function () {
