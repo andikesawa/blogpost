43284b7fb38c0f6f1210cdfce69b56b8012429fb
Andi Kesawa <andikesawa05@gmail.com>
2026-08-07 11:26:49 +0800

    Feature/about (#1)
    * feat: create AboutController
    
    * feat: setup/buat halaman about dengan metode blade templating engine, dan blade components

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>About page</title>
-</head>
-<body>
-  <h1>About Page</h1>
-</body>
-</html>
+<x-layout :title="$title">
+    <p>Welcome to about page</p>
+</x-layout>
