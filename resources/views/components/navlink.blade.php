aa58bfd46de75f8d56c2d9420f860c5c4fe49bad
Andi Kesawa <andikesawa05@gmail.com>
2026-08-10 20:55:18 +0800

    Feat: Membuat activelink (#4)

---
+++ resources/views/components/navlink.blade.php
@@ -0,0 +1,25 @@
+@props(['href', 'current' => false, 'ariaCurrent' => false])
+
+@php
+
+    //    $classes = $current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5'
+
+    if ($current) {
+        $classes = 'bg-gray-900 text-white';
+        $ariaCurrent = 'page';
+    } else {
+        $classes = 'text-gray-300 hover:bg-white/5 hover:text-white';
+    }
+@endphp
+
+
+<a href="{{ $href }}"
+    {{ $attributes->merge([
+        'class' => 'rounded-md px-3 py-2 text-sm font-medium ' . $classes,
+        'aria-current' => $ariaCurrent,
+    ]) }}>
+    {{ $slot }}
+</a>
+
+{{-- rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white --}}
+{{-- rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white --}}
