2f9018065012cc2e1e8a815d05f66d5377a777c1
andikesawa <andikesawa05@gmail.com>
2026-08-10 20:48:36 +0800

    Feat: Membuat activelink

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="bg-gray-800">
-  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
                 <div class="shrink-0">
@@ -9,14 +9,10 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                        <a href="/" aria-current="page"
-                            class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Home</a>
-                        <a href="/blog"
-                            class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Blog</a>
-                        <a href="/about"
-                            class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                        <a href="/contact"
-                            class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                        <x-navlink href="/" :current="request()->is('/')">Home</x-navlink>
+                        <x-navlink href="/blog" :current="request()->is('blog')">Blog</x-navlink>
+                        <x-navlink href="/about" :current="request()->is('about')">About</x-navlink>
+                        <x-navlink href="/contact" :current="request()->is('contact')">Contact</x-navlink>
                     </div>
                 </div>
             </div>
@@ -64,19 +60,15 @@
                 </button>
             </div>
         </div>
-    </div>  
+    </div>
 
-  <el-disclosure id="mobile-menu" hidden class="block md:hidden">
+    <el-disclosure id="mobile-menu" hidden class="block md:hidden">
         <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-            <a href="/" aria-current="page"
-                class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">Home</a>
-            <a href="/blog"
-                class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Blog</a>
-            <a href="/about"
-                class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-            <a href="/contact"
-                class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+            <x-navlink class='block' href="/" :current="request()->is('/')">Home</x-navlink>
+            <x-navlink class='block' href="/blog" :current="request()->is('blog')">Blog</x-navlink>
+            <x-navlink class='block' href="/about" :current="request()->is('about')">About</x-navlink>
+            <x-navlink class='block' href="/contact" :current="request()->is('contact')">Contact</x-navlink>
         </div>
         <div class="border-t border-white/10 pt-4 pb-3">
             <div class="flex items-center px-5">
@@ -111,5 +103,5 @@
                     out</a>
             </div>
         </div>
-    </el-disclosure>  
+    </el-disclosure>
 </nav>
